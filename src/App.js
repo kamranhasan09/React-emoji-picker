5dc2b8c783bbad6ede86529a98792432809cf9ca
kamranhasan09 <60890375+kamranhasan09@users.noreply.github.com>
2021-09-15 19:40:23 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Picker ,{SKIN_TONE_DARK} from 'emoji-picker-react'
+import { useState } from 'react';
 
 function App() {
+
+  const [chosenEmoji,setChosenEmoji] = useState(null);
+
+  const onEmojiClick = (event,emojiObject) =>{
+    setChosenEmoji(emojiObject);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1 style={{textAlign:'center'}}>Emoji Picker</h1>
+    <div style={{textAlign:'center',marginLeft:'510px'}}>
+      <Picker onEmojiClick = {onEmojiClick} skinTone={SKIN_TONE_DARK}/>
+      {chosenEmoji && <EmojiData chosenEmoji = {chosenEmoji}/>}
     </div>
+    </>
   );
 }
+
+
+const EmojiData = ({chosenEmoji}) =>(
+  <div style={{textAlign:'center',marginRight:'630px'}}>
+    <br></br>
+    <br></br>
+    <hr></hr>
+    <strong>Names:</strong>{chosenEmoji.names.join(', ')}<br/>
+    <strong>Symbol:</strong>{chosenEmoji.emoji}<br/>
+    
+  </div>
+  );
 
 export default App;
